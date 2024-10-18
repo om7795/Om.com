1e10e3da4ee9f1130e11900855c3f82e44f54f15
om7795 <om.7795uk@gmail.com>
2024-10-18 21:59:29 +0530

    index.js

---
+++ index.js
@@ -0,0 +1,36 @@
+window.onscroll = function() {myFunction()};
+
+var header = document.getElementById("myHeader");
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+
+
+
+
+function downloadCV() {
+    // Replace 'path-to-cv.pdf' with the actual path to your CV file
+    const link = document.createElement('a');
+    link.href = 'cv.pdf';
+    link.download = 'cv.pdf';
+    link.click();
+}
+
+
+// Disable right-click
+document.addEventListener('contextmenu', function (e) {
+  e.preventDefault();
+});
+
+// Disable F12 (Developer Tools)
+document.onkeydown = function (e) {
+  if (e.keyCode == 123) {
+     return false;
+  }
+};
